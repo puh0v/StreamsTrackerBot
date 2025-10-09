240ee2f710089fb338c53b33f1f1b6f6dd255f22
puh0v <puh7794@gmail.com>
2025-10-09 08:29:41 +0300

    feat(SubscriptionsRepository): добавлен метод findAllDistinctChannelIds()

--- src/main/java/io/github/puh0v/db/subscriptions/SubscriptionsRepository.java
+++ src/main/java/io/github/puh0v/db/subscriptions/SubscriptionsRepository.java
@@ -1,6 +1,7 @@
 package io.github.puh0v.db.subscriptions;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,9 @@ import java.util.Set;
 public interface SubscriptionsRepository extends JpaRepository<SubscriptionsEntity, Long> {
 
     boolean existsByUser_TelegramIdAndChannel_ChannelId(Long telegramId, String channelId);
+
+    @Query("select distinct s.channel.channelId from SubscriptionsEntity s")
+    Set<String> findAllDistinctChannelIds();
 
     Set<SubscriptionsEntity> findAllByChannel_ChannelId(String channelId);
 
