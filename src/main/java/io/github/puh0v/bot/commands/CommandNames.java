b4afd4b718e7ec52417e5978d21c6c5add27863c
puh0v <puh7794@gmail.com>
2025-10-20 13:44:15 +0300

    refactor(CommandNames): заменил статические константы на enum с возможностью получить содержимое констант;

--- src/main/java/io/github/puh0v/bot/commands/CommandNames.java
+++ src/main/java/io/github/puh0v/bot/commands/CommandNames.java
@@ -1,8 +1,20 @@
 package io.github.puh0v.bot.commands;
 
-public final class CommandNames {
-    public static final String START = "/start";
-    public static final String MY_SUBSCRIPTIONS = "/my_subscriptions";
-    public static final String SEND_LINK = "/send_link";
-    public static final String CANCEL = "/cancel";
+import lombok.Getter;
+
+@Getter
+public enum CommandNames {
+    START("/start"),
+    SEND_LINK("/send_link"),
+    MY_SUBSCRIPTIONS("/my_subscriptions"),
+    DELETE_SUBSCRIPTION("/delete_subscription"),
+    ADMIN_COMMAND("/admin_command"),
+    CANCEL("/cancel"),
+    PAGE("/PAGE_");
+
+    private String code;
+
+    private CommandNames(String code) {
+        this.code = code;
+    }
 }
